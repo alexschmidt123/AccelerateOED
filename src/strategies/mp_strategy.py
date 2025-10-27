24dda9cd677298b71111847de68110936eab4888
Gaoming Lin <gaominglins@gmail.com>
2025-10-27 13:40:44 -0500

    dgbs

--- src/strategies/mp_strategy.py
+++ src/strategies/mp_strategy.py
@@ -6,7 +6,7 @@ in coupled oscillator systems.
 """
 
 import time
-from core.mocu_cuda import *
+from src.core.mocu_cuda import *
 import torch
 import numpy as np
 from torch.nn import Sequential, Linear, ReLU, GRU
